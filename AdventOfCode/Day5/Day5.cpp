026ff440ef79f55c6d814a923ebae299537ad8a7
Devin Fink <devinfink007@gmail.com>
2023-12-05 00:19:12 -0700

    Finished all parsing logic for day 5

---
+++ AdventOfCode/Day5/Day5.cpp
@@ -0,0 +1,89 @@
+﻿// Day5.cpp : Defines the entry point for the application.
+//
+
+#include "Day5.h"
+
+using namespace std;
+vector<string> split(string str, char delimiter);
+
+struct inputMap
+{
+	vector<long long> destinations;
+	vector<long long> sources;
+	vector<long long> ranges;
+};
+
+int part1(vector<string> fileToVector)
+{
+	vector<inputMap> maps;
+	vector<long long> seeds;
+	for (string currentLine : fileToVector)
+	{
+		if (currentLine.find("seeds") != string::npos)
+		{
+			string trimmedLine = currentLine.substr(currentLine.find(" ") + 1);
+			for (string seed : split(trimmedLine, ' '))
+			{
+				seeds.push_back(strtoll(seed.c_str(), NULL, 10));
+			}
+			continue;
+		}
+
+		if ((currentLine.find("map") != string::npos))
+		{
+			inputMap newMap;
+			maps.push_back(newMap);
+		}
+		else if (currentLine.size() == 0)
+		{
+			continue;
+		}
+		else
+		{
+			vector<string> lineOfMappings = split(currentLine, ' ');
+			maps[maps.size() - 1].destinations.push_back(strtoll(lineOfMappings[0].c_str(), NULL, 10));
+			maps[maps.size() - 1].sources.push_back(strtoll(lineOfMappings[1].c_str(), NULL, 10));
+			maps[maps.size() - 1].ranges.push_back(strtoll(lineOfMappings[2].c_str(), NULL, 10));
+		}
+	}
+
+	return seeds[0];
+
+}
+
+vector<string> split(string str, char delimiter)
+{
+	vector<string> strings;
+	string currentString;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == delimiter)
+		{
+			strings.push_back(currentString);
+			currentString.clear();
+			continue;
+		}
+		else if (i == str.size() - 1)
+		{
+			currentString += str[i];
+			strings.push_back(currentString);
+		}
+		currentString += str[i];
+	}
+
+	return strings;
+}
+
+int main()
+{
+	ifstream fileToRead("C:\\Users\\devin\\source\\repos\\Advent-Of-Code-2023\\AdventOfCode\\Day5\\dayFiveInput.txt");
+	vector<string> fileToVector;
+	string currentLine;
+	while (getline(fileToRead, currentLine))
+	{
+		fileToVector.push_back(currentLine);
+	}
+
+	cout << part1(fileToVector);
+}
---
+++ AdventOfCode/Day5/Day5.h
@@ -0,0 +1,11 @@
+﻿// Day5.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+// TODO: Reference additional headers your program requires here.
