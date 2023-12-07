5d671ed2d1f6e050c6b524cfecd0c2f340ef1d03
Devin Fink <devinfink007@gmail.com>
2023-12-06 18:17:29 -0700

    Update Day 5

--- AdventOfCode/Day5/Day5.cpp
+++ AdventOfCode/Day5/Day5.cpp
@@ -13,10 +13,8 @@ struct inputMap
 	vector<long long> ranges;
 };
 
-int part1(vector<string> fileToVector)
+void populateVectors(vector<string> fileToVector, vector<inputMap>* maps, vector<long long>* seeds)
 {
-	vector<inputMap> maps;
-	vector<long long> seeds;
 	for (string currentLine : fileToVector)
 	{
 		if (currentLine.find("seeds") != string::npos)
@@ -24,7 +22,7 @@ int part1(vector<string> fileToVector)
 			string trimmedLine = currentLine.substr(currentLine.find(" ") + 1);
 			for (string seed : split(trimmedLine, ' '))
 			{
-				seeds.push_back(strtoll(seed.c_str(), NULL, 10));
+				seeds->push_back(strtoll(seed.c_str(), NULL, 10));
 			}
 			continue;
 		}
@@ -32,7 +30,7 @@ int part1(vector<string> fileToVector)
 		if ((currentLine.find("map") != string::npos))
 		{
 			inputMap newMap;
-			maps.push_back(newMap);
+			maps->push_back(newMap);
 		}
 		else if (currentLine.size() == 0)
 		{
@@ -41,13 +39,98 @@ int part1(vector<string> fileToVector)
 		else
 		{
 			vector<string> lineOfMappings = split(currentLine, ' ');
-			maps[maps.size() - 1].destinations.push_back(strtoll(lineOfMappings[0].c_str(), NULL, 10));
-			maps[maps.size() - 1].sources.push_back(strtoll(lineOfMappings[1].c_str(), NULL, 10));
-			maps[maps.size() - 1].ranges.push_back(strtoll(lineOfMappings[2].c_str(), NULL, 10));
+			(*maps)[maps->size() - 1].destinations.push_back(strtoll(lineOfMappings[0].c_str(), NULL, 10));
+			(*maps)[maps->size() - 1].sources.push_back(strtoll(lineOfMappings[1].c_str(), NULL, 10));
+			(*maps)[maps->size() - 1].ranges.push_back(strtoll(lineOfMappings[2].c_str(), NULL, 10));
+		}
+	}
+}
+
+int part1(vector<string> fileToVector)
+{
+	vector<inputMap> maps;
+	vector<long long> seeds;
+
+	populateVectors(fileToVector, &maps, &seeds);
+
+	for(inputMap map : maps)
+	{
+		for (int seed = 0; seed < seeds.size(); seed++)
+		{
+			for (int i = 0; i < map.destinations.size(); i++)
+			{
+				bool seedInRange;
+				if ((seeds[seed] >= map.sources[i]) && seeds[seed] < (map.sources[i] + map.ranges[i]))
+					seedInRange = true;
+				else
+					seedInRange = false;
+
+				if (seedInRange)
+				{
+					seeds[seed] = map.destinations[i] + (seeds[seed] - map.sources[i]);
+					break;
+				}
+			}
 		}
 	}
 
-	return seeds[0];
+	long long min = seeds[0];
+	for (long long seed : seeds)
+	{
+		if (seed < min)
+		{
+			min = seed;
+		}
+	}
+	return min;
+}
+
+int part2(vector<string> fileToVector)
+{
+	vector<inputMap> maps;
+	vector<long long> seeds;
+
+	populateVectors(fileToVector, &maps, &seeds);
+
+	vector<long long> newSeeds;
+	for (int i = 0; i < seeds.size(); i += 2)
+	{
+		for (int j = 0; j <  seeds[i + 1]; j++)
+		{
+			newSeeds.push_back(seeds[i] + j);
+		}
+	}
+
+	for (inputMap map : maps)
+	{
+		for (int seed = 0; seed < newSeeds.size(); seed++)
+		{
+			for (int i = 0; i < map.destinations.size(); i++)
+			{
+				bool seedInRange;
+				if ((newSeeds[seed] >= map.sources[i]) && newSeeds[seed] < (map.sources[i] + map.ranges[i]))
+					seedInRange = true;
+				else
+					seedInRange = false;
+
+				if (seedInRange)
+				{
+					newSeeds[seed] = map.destinations[i] + (newSeeds[seed] - map.sources[i]);
+					break;
+				}
+			}
+		}
+	}
+
+	long long min = newSeeds[0];
+	for (long long seed : newSeeds)
+	{
+		if (seed < min)
+		{
+			min = seed;
+		}
+	}
+	return min;
 
 }
 
@@ -85,5 +168,6 @@ int main()
 		fileToVector.push_back(currentLine);
 	}
 
-	cout << part1(fileToVector);
+	cout << part1(fileToVector) << endl;
+	cout << part2(fileToVector);
 }
